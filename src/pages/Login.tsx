f396692caf4e3cc5041b1b5790a5affd976b2ddd
Adrian Czech <czech400m@gmail.com>
2024-01-25 19:08:57 +0100

    add bootstrap, polish login page and cleanup some stuff from vite's template

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {Button, Form} from "react-bootstrap";
 
 const Login: React.FC = () => {
-    const [credentials, setCredentials] = useState({ username: '', password: '' });
+    const [credentials, setCredentials] = useState({email: '', password: ''});
     const navigate = useNavigate();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setCredentials({ ...credentials, [name]: value });
+        const {type, value} = e.target;
+        setCredentials({...credentials, [type]: value});
     };
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         try {
+            console.log(credentials);
             // dummy jwt token
-            const response = {data: { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gVHJhZGVyIiwiaWF0IjoxNTE2MjM5MDIyfQ.AYPl1SZTtF1mxhZi28-RexyDvvgJiqGoiHP_suS4FO4"}};
+            const response = {data: {token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gVHJhZGVyIiwiaWF0IjoxNTE2MjM5MDIyfQ.AYPl1SZTtF1mxhZi28-RexyDvvgJiqGoiHP_suS4FO4"}};
             if (response.data && response.data.token) {
 
                 localStorage.setItem('token', response.data.token);
@@ -29,32 +31,28 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={credentials.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <section className="d-flex justify-content-center align-items-center vh-100 vw-100">
+            <div className="login-inner-element d-flex flex-column shadow-lg bg-white rounded">
+                <h2 className="d-flex justify-content-center mb-4">Login</h2>
+                <Form className="d-flex flex-column" onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onChange={handleChange} type="email" placeholder="Enter email"/>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control onChange={handleChange} type="password" placeholder="Password"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Sign in
+                    </Button>
+                    <Form.Text className="text-muted">
+                        {/*//TODO: link to be signed up to be added*/}
+                        Don't have an account? <Link to={"/"}>Sign up</Link>
+                    </Form.Text>
+                </Form>
+            </div>
+        </section>
     );
 };
 
